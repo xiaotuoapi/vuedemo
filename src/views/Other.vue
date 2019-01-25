2445fdcb6fc05ad203c929c8735416ccdaf146d2
Liu Tuo <tuo.liu@xor-media.com>
2019-01-25 16:18:17 +0800

    hello

--- src/views/Other.vue
+++ src/views/Other.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="other">
-    <h1>This is an other page </h1>
-  </div>
+<div id='app1'>
+<button v-if='showBtn' v-on:click='handleClick'>Click me</button>
+</div>
 </template>
+<script>
+new Vue ({
+el :'#app', 
+data : {
+showBtn: true 
+},
+methods: {
+  handleClick: function () {
+  console.log ( 'Clicked !'); 
+  }
+}
+})
+</script> 
